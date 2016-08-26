ad54ad8641ae81f0bfb4bc6233eeee3a63b7694e
Wolfram Hempel <wolfram.hempel@hoxton-one.com>
2016-08-26 16:10:12 +0200

    dashboard done

--- dashboard/frontend/src/start.js
+++ dashboard/frontend/src/start.js
@@ -1,3 +1,6 @@
 $(function(){
-	ko.applyBindings( new ViewModel() );
+	window.kot = new KoTools( ko );
+	var ds = deepstream( 'ec2-54-154-140-90.eu-west-1.compute.amazonaws.com:6020' ).login( null, function(){
+		ko.applyBindings( new ViewModel( ds ) );
+	});
 });
--- dashboard/frontend/src/viewmodel.js
+++ dashboard/frontend/src/viewmodel.js
@@ -1,26 +1,69 @@
 class ViewModel{
-	constructor() {
+	constructor( ds ) {
+		this._ds = ds;
+		this._ds.record.listen( 'metrics/*', this._onMetric.bind( this ) );
 		this.pricesInLastSecond = ko.observable( 0 );
 		this.pricesSent = ko.observable( 0 );
 		this.clientsCount = ko.observable( 0 );
 		this.providerCount = ko.observable( 0 );
-		this.serverCount = ko.observable( 0 );
+		this.serverCount = ko.observable( 1 );
 		this.requestCount = ko.observable( 0 );
 		this.lastUpdated = ko.observable( 'NEVER' );
+		this.totalProbes = ko.observable( 0 );
+		this.activeProbes = ko.observable( 0 );
+		this.probes = ko.observableArray();
 		this._lastUpdateTime = null;
-		this._getData();
+		this._updateInterval = setInterval( this._updateTotals.bind( this ), 1000 );
 	}
 
-	_getData() {
-		$.getJSON( 'http://localhost:8000', this._onUpdate.bind( this ) );
-		setTimeout( this._getData.bind( this ), 1000 );
+	_onMetric( metric, isSubscribed ) {
+		var probe = this.probes().filter( probe => { return probe.name() === metric; })[ 0 ];
+
+		if( isSubscribed && !probe ) {
+			this.probes.push( new Probe( this._ds, metric ) );
+		}
+
+		if( !isSubscribed && probe ) {
+			this.probes.slice( this.probes.indexOf( probe ), 1 );
+			probe.destroy();
+		}
 	}
 
-	_onUpdate( data ) {
-		this.pricesInLastSecond( data.prices_received );
-		this.clientsCount( data.clients_count );
-		this.pricesSent( data.prices_sent );
-		this.providerCount( data.provider_count );
-		this.requestCount( this.requestCount() + 1 );
+
+	_updateTotals() {
+		var pricesInLastSecond = 0;
+		var pricesSent = 0;
+		var clientsCount = 0;
+		var providerCount = 0;
+		var total = 0;
+		var active = 0;
+		var probes = this.probes();
+		var i;
+
+		for( i = 0; i < probes.length; i++ ) {
+			total++;
+
+			if( !probes[ i ].active() ) {
+				continue;
+			}
+
+			active++;
+
+			if( probes[ i ].runMode === 'provider' ) {
+				providerCount++;
+				pricesSent += probes[ i ].count();
+			} else {
+				clientsCount++;
+				pricesInLastSecond += probes[ i ].count();
+			}
+		}
+
+
+		this.pricesInLastSecond( pricesInLastSecond );
+		this.clientsCount( clientsCount );
+		this.pricesSent( pricesSent );
+		this.providerCount( providerCount );
+		this.totalProbes( total );
+		this.activeProbes( active );
 	}
 }
