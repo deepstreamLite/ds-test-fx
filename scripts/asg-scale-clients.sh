f4897652bc58cea45118d0c4d1c06e81d3ed8c55
yasserf <yasser.fadl@gmail.com>
2016-09-02 11:05:22 +0200

    Adding client/server scripts

---
+++ scripts/asg-scale-clients.sh
@@ -0,0 +1,1 @@
+aws autoscaling set-desired-capacity --auto-scaling-group-name ds-cluster-test-fx-clients --desired-capacity $1
---
+++ scripts/ecs-scale-clients.sh
@@ -0,0 +1,1 @@
+aws ecs update-service --cluster deepstream-perf-clients --service clients --desired-count $1
