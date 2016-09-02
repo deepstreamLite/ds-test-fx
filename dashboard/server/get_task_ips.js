94c27cac2319b49ca59aab98aa59434b1ca4207c
John Diamond <johndiamond@gmx.co.uk>
2016-09-02 16:53:57 +0200

    fixed get tasks

--- dashboard/server/get_task_ips.js
+++ dashboard/server/get_task_ips.js
@@ -4,18 +4,15 @@ const ec2 = new AWS.EC2({region: 'eu-west-1'});
 const deepstream = require( 'deepstream.io-client-js' )
 
 const CLUSTER = 'deepstream-perf-cluster';
+const SERVICE = 'deepstream-server'
 
 const ds = deepstream('52.210.144.102:6021')
 ds.login();
 
-setInterval(updateServerRecord, 60000);
-updateServerRecord();
-
 const serverRecord = ds.record.getRecord('servers');
 
-function getIP() {
-	return ecs.listContainerInstances({ cluster: CLUSTER  }).promise()
-		.then(data => ecs.describeContainerInstances({ containerInstances: data.containerInstanceArns, cluster: CLUSTER  }).promise())
+function getIP(containerArns) {
+	return ecs.describeContainerInstances({ containerInstances: containerArns, cluster: CLUSTER  }).promise()
 		.then(data => {
 			var i_ids = data.containerInstances.map(d => { return d.ec2InstanceId })
 			//console.log(i_ids);
@@ -26,16 +23,57 @@ function getIP() {
 		} );
 }
 
-function updateServerRecord() {
-	console.log( 'Updating Server Record' );
-	getIP().then(data => {
-		//console.log(JSON.stringify(data));
-		serverRecord.set(
-			'ips',
-			data.Reservations[0].Instances.map(res => {
-				return res.PublicIpAddress;
-			})
-		);
-		console.log(serverRecord.get());
+function listServerIps(){
+	var params = {
+		cluster: CLUSTER,
+		serviceName: SERVICE
+	};
+
+	ecs.listTasks(params, function(err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else	 {
+			//console.log(data);		   // successful response
+			describeTasks(data.taskArns);
+		}
+
 	});
 }
+
+function describeTasks(taskArns){
+	ecs.describeTasks({
+		tasks: taskArns,
+		cluster: CLUSTER
+	}, function(err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else     {
+			getIP(data.tasks.map(data => {
+				return data.containerInstanceArn;
+			})).then(updateServerRecord)
+			//console.log(data);           // successful response
+		}
+
+	});
+
+}
+
+
+
+function updateServerRecord(data) {
+	console.log( 'Updating Server Record' );
+
+	//console.log(JSON.stringify(data));
+	serverRecord.set(
+			'ips',
+			data.Reservations.map( res => {
+				return res.Instances.map(res => {
+					return res.PublicIpAddress;
+				})
+			}).reduce((prev, curr) =>{
+				return prev.concat(curr);
+			})
+			);
+	console.log(serverRecord.get());
+}
+
+setInterval(listServerIps, 60000);
+listServerIps();
